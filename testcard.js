17a8631c0556b261e0ee42db472979bbcb19d89d
Manish Konduri <manish.konduri@pramati.com>
2019-12-18 18:24:58 +0530

    Third commit

--- testcard.js
+++ testcard.js
@@ -1,26 +1,51 @@
-var i = 1
+var j = 6;
+var d2;
+var k=0;
 const app = document.getElementById('root');
-function calling() {
+function calling(loadmore) {
     var xhr = new XMLHttpRequest();
     var v = "https://randomuser.me/api/?results=100"
     xhr.open('GET', v, true);
     xhr.onload = function () {
+        loading();
+        console.log(v)
         var d = JSON.parse(this.response);
-        var d2 = d["results"];
-        if (d2.length !== 0) {
-            for (var i = 0; i < 12; i++) {
-                addingData(d2[i])
-            }
+        d2 = d["results"];
+        for(i=0;i<6;i++){
+            addingData(d2[i]);
         }
-        else {
-            alert("This is the last page available!!!!!")
-            document.getElementById('button').style.visibility = "hidden";
-        }
+        // else {
+        //     alert("This is the last page available!!!!!")
+        //     document.getElementById('button').style.visibility = "hidden";
+        // }
     }
 
     xhr.send();
-    i = i + 1;
+ 
 }
+function calculating(){
+    for(var i=j;i<j+6;i++)
+    {
+        if(i== 95){
+            j=6;
+            calling();
+        }
+        console.log(i)
+        addingData(d2[i]);
+    }
+    j+=6;
+}
+function loading(){
+    document.getElementById("loader").style.visibility = "hidden";
+}
+window.onscroll = function() {
+    this.console.log(window.innerHeight+" "+ window.scrollY+" "+document.body.scrollHeight)
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+        document.getElementById("loader").style.visibility = "visible";
+        calculating()
+
+    }
+};
 function addingData(i){
     const card = document.createElement('div')
                     card.setAttribute('class', 'card')
@@ -28,14 +53,34 @@ function addingData(i){
                     const details = document.createElement('div')
                     const image = document.createElement('img')
                     const imagereplace = document.createElement('h2')
-                    // i.avatar=null
+                    // i.picture.large=null
                     if (i.picture.large) {
                         image.src = i.picture.large;
                         image.setAttribute('class', 'img')
                     }
                     else {
                         iNA=document.createElement('div')
-                        iNA.textContent=i.name.first[0].toUpperCase()+ i.name.last[0].toUpperCase()
+                        var fn="";
+                        var ln="";
+                        // i.name.first=null
+                        // //  i.name.last=null
+                        if(i.name.first ==null && i.name.last==null){
+                            fn="N"
+                            ln="A"
+                        }
+                        else if(i.name.first==null && i.name.last!==null ){
+                            fn=" "
+                        }
+                        else{
+                            fn=i.name.first[0];
+                        }
+                        if(i.name.first!==null && i.name.last==null){
+                            ln=" "
+                        }
+                        else{
+                            ln=i.name.last[0];
+                        }
+                        iNA.textContent=fn+ ln
                         iNA.setAttribute('class','profileImage')
                         details.appendChild(iNA)
 
@@ -44,8 +89,16 @@ function addingData(i){
                     details.setAttribute('class', 'container')
     
                     const h1 = document.createElement('h1')
+                    var fn="";
+                    var ln="";
+                    // i.name.first=""
+                    // i.name.last=""
+                    if(i.name.first==null && i.name.last==null){
+                        h1.textContent = "Name Not" + " " + "Mentioned"
+                    }
+                    else{
                     h1.textContent = i.name.first + " " + i.name.last
-    
+                    }
                     const p = document.createElement('p')
                     p.textContent = i.email
     
